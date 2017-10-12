6d19e422dbd9cc33d7f1f1b6e27e2ff46d88bc75
juxiaona <415280786@qq.com>
2017-10-12 21:00:24 +0800

    V2.0

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 from common.op_mysql import Op_Mysql
 
 op=Op_Mysql()
-op.db_conn()
+sql="insert into user values(7,'jxn');"
+op.execute_sql(sql)
+sql1='select * from user;'
+result=op.get_all(sql1)
+print(result)
